1192cf32dc06b2ee07a1a8d2999ab1a2a8ea7297
EyeanL <95993002+EyeanL@users.noreply.github.com>
2024-02-17 18:00:52 -0500

    3

--- arduinowork/arduinowork.ino
+++ arduinowork/arduinowork.ino
@@ -10,17 +10,21 @@ void loop(){
         Serial.print("Recieved Value: ");
         Serial.println(input);
         int ival = input.toInt();
+
+        // Activate user
+        vibration_fb(ival);
+        matrixled_fb(ival);
     }
 }
 
-void vibration_fb(double distance, double speed){
+void vibration_fb(double distance){
     digitalWrite(vibrating_fb_pin, HIGH);
     delay((int)(1/distance));
     digitalWrite(vibrating_fb_pin, LOW);
     delay((int)(1/distance));
 }
 
-void matrixled_fb(double distance, double speed){
+void matrixled_fb(double distance){
 
 }
 
